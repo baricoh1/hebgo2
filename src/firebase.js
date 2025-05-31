59bc443cb91d8a2b98fbba8d826140a85053a1a8
baricoh1 <baricoh1@gmail.com>
2025-05-31 17:26:52 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,22 @@
-// src/firebase.js
+// firebase.js
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyA0P7Epgzq3yFPeA4b6MiNW8Omk_vH_iGc",
   authDomain: "hebgo-38419.firebaseapp.com",
   projectId: "hebgo-38419",
-  storageBucket: "hebgo-38419.firebasestorage.app",
+  storageBucket: "hebgo-38419.appspot.com", // <- תיקנתי לך פה גם
   messagingSenderId: "737678858666",
   appId: "1:737678858666:web:29fc2bd917d31849cdf3d3",
   measurementId: "G-YE7PG8TYSP"
 };
 
+// יצירת אפליקציה
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+
+// יצירת חיבור ל-Firestore
+const db = getFirestore(app);
+
+// 🔁 כאן החשוב:
+export { app, db };
