9634ad60a4fd241e62d70b5e4e7826344a9d8d5a
baricoh1 <baricoh1@gmail.com>
2025-05-31 17:01:12 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,14 @@
-// Import the functions you need from the SDKs you need
-import { getFirestore } from "firebase/firestore";
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// src/firebase.js
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyA0P7Epgzq3yFPeA4b6MiNW8Omk_vH_iGc",
   authDomain: "hebgo-38419.firebaseapp.com",
   projectId: "hebgo-38419",
-  storageBucket: "hebgo-38419.firebasestorage.app",
+  storageBucket: "hebgo-38419.appspot.com", // תקן ל-appspot.com, לא .app
   messagingSenderId: "737678858666",
   appId: "1:737678858666:web:29fc2bd917d31849cdf3d3",
   measurementId: "G-YE7PG8TYSP"
@@ -20,7 +16,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
 
-
-
+export { db };
