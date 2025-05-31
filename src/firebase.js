de6de1a10a9e0a6af38e5c6da89f9d1f487f3752
baricoh1 <baricoh1@gmail.com>
2025-05-31 17:21:21 +0300

    1

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,18 @@
 // src/firebase.js
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
-// Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyA0P7Epgzq3yFPeA4b6MiNW8Omk_vH_iGc",
   authDomain: "hebgo-38419.firebaseapp.com",
   projectId: "hebgo-38419",
-  storageBucket: "hebgo-38419.appspot.com", // תקן ל-appspot.com, לא .app
+  storageBucket: "hebgo-38419.firebasestorage.app",
   messagingSenderId: "737678858666",
   appId: "1:737678858666:web:29fc2bd917d31849cdf3d3",
   measurementId: "G-YE7PG8TYSP"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };
+export const db = getFirestore(app);
+export const auth = getAuth(app);
